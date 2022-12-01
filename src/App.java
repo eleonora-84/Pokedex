9606be1930dfbd164d69817e21b8f6c40db7a89f
eleonora-84 <eleonora.pisacane@yahoo.it>
2022-12-01 17:35:19 +0100

    ultimo punto

--- src/App.java
+++ src/App.java
@@ -3,22 +3,22 @@ import model.Pokemon;
 
 public class App {
     public static void main(String[] args) throws Exception {
-                
+         /*       
         PokemonDatabase pokemonDB = new PokemonDatabase();
         
         Pokemon p1 = new Pokemon("Pikachu", "electricity", 20);
-        // Pokemon p2 = new Pokemon("Bulbasaur", "grass", 18);
+        Pokemon p2 = new Pokemon("Bulbasaur", "grass", 18);
         Pokemon p3 = new Pokemon("Charmender", "fire", 15);
-        // Pokemon p4 = new Pokemon("Squirtle", "water", 25);
+        Pokemon p4 = new Pokemon("Squirtle", "water", 25);
         
-        // pokemonDB.createPokemon(p1);
-        // pokemonDB.createPokemon(p2);
-        // pokemonDB.createPokemon(p3);
-        // pokemonDB.createPokemon(p4);
-
+        pokemonDB.createPokemon(p1);
+        pokemonDB.createPokemon(p2);
+        pokemonDB.createPokemon(p3);
+        pokemonDB.createPokemon(p4);
+        */
         // pokemonDB.getPokemonByID(3);
         
-        
+        /*
         System.out.println(pokemonDB.getAllPokemon());
        
 
@@ -36,8 +36,11 @@ public class App {
         
         pokemonDB.closeConnection();
         
+        */
+        // System.out.println(pokemonDB.getAllPokemon());
+        
         ConvertPokemon.convertFromList2Array();
-    
+        ConvertPokemon.multiplyDivideID();
      
     }
 }
--- src/model/ConvertPokemon.java
+++ src/model/ConvertPokemon.java
@@ -1,18 +1,18 @@
 package model;
 
-import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import persistance.util.DatabaseConnector;
 import persistance.util.SqlQueryStorage;
 
 // Utilizzando il codice scritto in precedenza, scrivere un metodo statico chiamato "convertFromList2Array"
 // che dato un ArrayList lo converte in un array di interi con gli indici che sono stati letti da un database.
+
+//+ Scrivere un metodo che moltiplichi per 2 gli ID che hanno un indice pari e che divida per 3 quelli dispari.
 
 public class ConvertPokemon {
 
@@ -31,14 +31,49 @@ public class ConvertPokemon {
 
       while (resultSet.next()) {
         int idResult = resultSet.getInt("id");
-
         listaPokemonID.add(idResult);
       }
+
       myArray = listaPokemonID.toArray();
-      System.out.println("La mia lista di id di Pokemon: " + Arrays.toString(myArray));
+      System.out.println(
+        "La mia lista di id di Pokemon: " + Arrays.toString(myArray)
+      );
     } catch (SQLException e) {
       e.printStackTrace();
     }
-    
+  }
+
+  public static void multiplyDivideID() {
+    ArrayList<Integer> listaPokemonID = new ArrayList<>();
+
+    try {
+      PreparedStatement preparedStatement = connection.prepareStatement(
+        SqlQueryStorage.getAllPokemon
+      );
+
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      while (resultSet.next()) {
+        int idResult = resultSet.getInt("id");
+
+        listaPokemonID.add(idResult);
+      }
+      for (int i = 0; i < listaPokemonID.size(); i++) {
+        int k = listaPokemonID.get(i);
+        if (k % 2 == 0) {
+          int id = listaPokemonID.get(i);
+          id = id * 2;
+          listaPokemonID.set(i, id);
+        } else {
+          int id = listaPokemonID.get(i);
+          id = id / 3;
+
+          listaPokemonID.set(i, id);
+        }
+      }
+      System.out.println(listaPokemonID);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 }
